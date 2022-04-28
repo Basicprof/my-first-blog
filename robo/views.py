ee80e2c20ecdb8bedd6ca60f9b956484e304b957
admin_forum <basicprof@gmail.com>
2022-04-28 17:02:52 +0300

    modified robo/wiews.py

--- robo/views.py
+++ robo/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'robo/post_list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',publish__year=year, publish__month=month, publish__day=day)
+     return render(request, 'robo/post_list.html', {'post': post})
