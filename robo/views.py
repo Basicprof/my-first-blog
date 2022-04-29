6e2fce375eb2b14929db2b115a5ab728f92da368
admin_forum <basicprof@gmail.com>
2022-04-29 13:17:20 +0300

    ьÑmigrations basic api create basic_api REST DJANGO DRF

--- robo/views.py
+++ robo/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -8,4 +9,4 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',publish__year=year, publish__month=month, publish__day=day)
-     return render(request, 'robo/post_list.html', {'post': post})
+    return render(request, 'robo/post_detail.html', {'post': post})
