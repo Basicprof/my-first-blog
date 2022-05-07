c7800a199781358e92dc7c9fdff181022971194b
admin_forum <basicprof@gmail.com>
2022-05-07 14:59:05 +0300

    capital

--- capital/views.py
+++ capital/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Cocos
 # Create your views here.
+def post_list(request):
+    posts = Cocos.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'capital/post_list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Cocos, slug=post, status='published',publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'capital/post_detail.html', {'post': post})
--- site_Robot/urls.py
+++ site_Robot/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('capital.urls')),
     path('account/', include('account.urls')),
+    path('admin/', admin.site.urls),
+    path('capital/', include('capital.urls')),
     path('robo/', include('robo.urls')),
     path('rest/', include('basic_api.urls')),
 ]
